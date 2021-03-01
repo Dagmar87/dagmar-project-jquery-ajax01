3d377b62328d6b957134a3a88ddc008618947be1
José Dagmar Florentino da Silva Sobrinho <64164006+Dagmar87@users.noreply.github.com>
2021-03-01 17:05:52 -0300

    updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //alert("funcionou");
 
 function consultaCep(){
+    $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep + "/json/";
     //console.log(cep);
@@ -11,6 +12,13 @@ function consultaCep(){
         success: function(response){
             console.log(response);
             $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP " + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").hide();
+            //$("#logradouro").html(response.logradouro);
             //document.getElementById("logradouro").innerHTML = response.logradouro;
             //document.getElementById("bairro").innerHTML = response.bairro;
             //document.getElementById("localidade").innerHTML = response.localidade;
@@ -19,3 +27,8 @@ function consultaCep(){
         }
     })
 }
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+})
